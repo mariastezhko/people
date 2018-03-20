442c6cd19c7a6993f730d08e2396ec23db2df87f
Maria Stezhko <stezhko@gmail.com>
2018-03-20 10:14:58 -0700

    feat: Connect second View Controller and display films

---
+++ People/FilmsViewController.swift
@@ -0,0 +1,150 @@
+//
+//  FilmsViewController.swift
+//  People
+//
+//  Created by Maria Stezhko on 3/19/18.
+//  Copyright © 2018 Maria Stezhko. All rights reserved.
+//
+
+import UIKit
+
+class FilmsViewController: UITableViewController {
+    
+    var films = [String]()
+
+    @IBOutlet var filmsTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        let url = URL(string: "http://swapi.co/api/films/")
+        // create a URLSession to handle the request tasks
+        let session = URLSession.shared
+        // create a "data task" to make the request and run the completion handler
+        let task = session.dataTask(with: url!, completionHandler: {
+            // see: Swift closure expression syntax
+            data, response, error in
+            //print("in here")
+            // see: Swift nil coalescing operator (double questionmark)
+            //print(data ?? "no data") // the "no data" is a default value to use if data is nil
+            do {
+                // try converting the JSON object to "Foundation Types" (NSDictionary, NSArray, NSString, etc.)
+                if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
+                    //print(jsonResult)
+                    if let results = jsonResult["results"] {
+                        let resultsArray = results as! NSArray
+                        //print(resultsArray.count)
+                        //print(resultsArray[0])
+                        for i in 0..<resultsArray.count {
+                            let resultsArray2 = resultsArray[i] as! NSDictionary
+                            //print(resultsArray.firstObject)
+                            //print(resultsArray2["name"])
+                            let name = resultsArray2["title"]
+                            if name != nil {
+                                self.films.append(name as! String)
+                                print(name as! String)
+                            }
+                            //let indexPath = IndexPath(row: i, section: 0)
+                            //self.tableView.cellForRow(at: indexPath)
+                        }
+                        DispatchQueue.main.async {
+                            self.filmsTableView.reloadData()
+                        }
+                        
+                    }
+                }
+            } catch {
+                print(error)
+            }
+        })
+        // execute the task and wait for the response before
+        // running the completion handler. This is async!
+        task.resume()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // if we return - sections we won't have any sections to put our rows in
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // return the count of people in our data array
+        return films.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Create a generic cell
+        let cell = UITableViewCell()
+        // set the default cell label to the corresponding element in the people array
+        cell.textLabel?.text = films[indexPath.row]
+        // return the cell so that it can be rendered
+        return cell
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
